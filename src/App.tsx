5090e618663c31ef95c22d13cb6c5fc8e3ca932c
egor_dikanskiy <egr.mr@yandex.ru>
2025-10-04 15:27:43 +0300

    renaming

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import RootLayout from "./components/RootLayout";
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { routerUrls } from "./config/routerUrls";
-import Catalog from "./pages/Catalog";
-import Map from "./pages/Map";
+import CatalogPage from "./pages/CatalogPage";
+import MapPage from "./pages/MapPage";
 
 function AppContent() {
   return (
     <RootLayout>
       <Routes>
-        <Route path={routerUrls.root} element={<Catalog />} />
-        <Route path={routerUrls.map} element={<Map />} />
+        <Route path={routerUrls.root} element={<CatalogPage />} />
+        <Route path={routerUrls.map} element={<MapPage />} />
       </Routes>
     </RootLayout>
   );
